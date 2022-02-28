bef43ff7e8465055a3214ccd766d6063b0dd9b15
Otakupog <100244422+Otakupog@users.noreply.github.com>
2022-02-28 16:35:09 -0600

    Initial commit

---
+++ source/Options.hx
@@ -0,0 +1,71 @@
+package;
+
+import lime.app.Application;
+import lime.system.DisplayMode;
+import flixel.util.FlxColor;
+import Controls.KeyboardScheme;
+import flixel.FlxG;
+import openfl.display.FPS;
+import openfl.Lib;
+
+class Options
+{
+    public function new()
+	{
+		display = updateDisplay();
+	}
+	
+	private var description:String = "";
+	private var display:String;
+	private var acceptValues:Bool = false;
+
+	public var acceptType:Bool = false;
+
+	public var waitingType:Bool = false;
+
+	public final function getDisplay():String
+	{
+		return display;
+	}
+
+	public final function getAccept():Bool
+	{
+		return acceptValues;
+	}
+
+	public final function getDescription():String
+	{
+		return description;
+	}
+
+	public function getValue():String
+	{
+		return updateDisplay();
+	};
+
+	public function onType(text:String)
+	{
+	}
+
+	// Returns whether the label is to be updated.
+	public function press():Bool
+	{
+		return true;
+	}
+
+	private function updateDisplay():String
+	{
+		return "";
+	}
+
+	public function left():Bool
+	{
+		return false;
+	}
+
+	public function right():Bool
+	{
+		return false;
+	}
+}
+
---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			FlxG.openURL("https://ninja-muffin24.itch.io/funkin");
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
